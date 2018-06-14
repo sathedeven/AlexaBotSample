cc53564e79c9d4e28ee18eb1b6bebb6d758b75d4
deven <sathedeven@gmail.com>
2018-06-14 17:31:08 +0530

    First commit

---
+++ Controllers/AlexaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace SimpleEchoBot.Controllers
+{
+    [Route("api/alexa")]
+    public class AlexaController : ApiController
+    {
+       
+        [HttpGet]
+        [HttpHead]
+        public IHttpActionResult root()
+        {
+            return this.Ok("Im Alive");
+        }
+
+        // POST api/values
+        //[RequireHttps]
+       
+        [HttpPost]
+        public HttpResponseMessage Post()
+        {
+            var speechlet = new SampleSessionSpeechlet();
+            return speechlet.GetResponse(this.Request);
+        }
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=webappbotdev001-Web-Deploy.pubxml -p:Password=9ihGNJZNJ8tzMXc0uNbKLLrlGh2y79mjwvcBoBrWorkgnD0kMYrS1sbecudJ
+
